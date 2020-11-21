a3b5b1f0f38225b8cca617bcb20dacb99f2c8957
Tatana <burmatova@inbox.ru>
2020-11-21 19:41:52 +0300

    Fixed build.gradle with JUnit4 and tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,41 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldCalculateAmountOfAditionBuyIfBuyUnder1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 900;
+        int actual = 100;
+        int expected = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateAmountOfAditionBuyIfBuyAfter1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1450;
+        int actual = 550;
+        int expected = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateAmountOfAditionBuyIfBuyEquales1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+        int actual = 0;
+        int expected = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+}
